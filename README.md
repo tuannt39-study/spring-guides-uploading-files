960c11e0380224a95aa73aadc77480b9a5f09afa
teo <tuannt39@gmail.com>
2018-01-14 14:25:45 +0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# gs-uploading-files
+
+- IDE: Intellij
+- http://spring.io/guides/gs/uploading-files/
+
+Simple CRUD app using:
+- Spring Boot
+- Thymeleaf
---
+++ src/main/java/vn/its/gsuploadingfiles/storage/StorageProperties.java
@@ -0,0 +1,21 @@
+package vn.its.gsuploadingfiles.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "src/main/resources/static/upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
